1732ed2cfe6eafba3130ae2ad3e82d8384ccddf4
Danielle Walraven <dwalraven21@gmail.com>
2021-04-18 13:32:26 -0400

    text update working

--- app.js
+++ app.js
@@ -21,17 +21,34 @@ const generateNewCountry = () => {
     fetchData()
   }
   // generate a random number between 0 and 249
+  // use the country at that index
+  const country = data[Math.floor(Math.random() * 250)]
   // create the variables
-  // name = data.name
-  // region = data.region
-  // population = data.population
-  // capital = data.capital
-  // language = data.languages
+  const name = country.name
+  const region = country.region
+  const population = country.population
+  const capital = country.capital
+
   //(this one requires us to loop through the array of languages and convert to a string.
   // we might want to change the text slightly if there is more than one language.)
-  //
-  // Set countryName to {name}
-  // Set countryText to `This country in the {region} region has a population size of {population}. The language(s) spoken here is/are {language}. The nation's capital is {capital}.`
+  let languages = country.languages[0].name
+  const numOfLanguages = country.languages.length
+  if (numOfLanguages !== 1) {
+    country.languages.forEach((newLanguage, index) => {
+      if (index !== 0 && index === numOfLanguages - 1) {
+        languages = languages + ", " + newLanguage.name
+      } else if (index === numOfLanguages - 1) {
+        languages = languages + " and " + newLanguage.name
+      }
+    })
+  }
+
+ countryName.innerText = name
+ countryText.innerText = `This country in the ${region} region has a population size of ${population}. The language${
+   numOfLanguages === 1 ? "" : "s"
+ } spoken here ${
+   numOfLanguages === 1 ? "is" : "are"
+ } ${languages}. The nation's capital is ${capital}.`
 }
 
 fetchData()
