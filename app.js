cb17d7569924b2bf3c65a8a090b9bd72f299c91a
Danielle Walraven <dwalraven21@gmail.com>
2021-04-18 12:50:20 -0400

    fixed mistakes

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
-const button = window.getElementById("fetchCountryButton")
-const countryName = window.getElementById("countryName")
-const countryText = window.getElementById("countryText")
-
-myButton.addEventListener('click', fetchData)
+const button = document.getElementById("fetchCountryButton")
+const countryName = document.getElementById("countryName")
+const countryText = document.getElementById("countryText")
 
 const fetchData = () => {
   fetch("https://restcountries.eu/rest/v2/all")
     .then((res) => res.json())
     .then((res) => console.log(res))
-    .catch((err) => console.log(err))
+    .catch((err) => {
+      console.log(err)
+      alert('API Could not be reached at this time')
+    })
 }
+
+button.addEventListener("click", fetchData)
